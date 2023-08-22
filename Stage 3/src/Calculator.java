0aa31be5d3a91970276101f2b9f132100d7b99c1
Geek Mover <92265150+geek-mover@users.noreply.github.com>
2023-08-22 23:38:19 +0300

    Update Calculator.java

--- Stage 3/src/Calculator.java
+++ Stage 3/src/Calculator.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
-public class Calculator {
-        public static void main(String[] args) {
-        System.out.println("");
-        System.out.println("|<>[+]<>[-]<>(◕‿◕)<>[*]<>[/]<>|");
-        System.out.println("Консольный калькулятор на Java.");
-        System.out.println("");
+public class Main {
+    public static String calc(String input) {
+        return input;
+    }
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("(◕‿◕) Введите выражение: ");
+        System.out.print("Input: ");
         String input = scan.nextLine();
         scan.close();
         String[] elements = input.split(" ");
-        if (elements.length != 3) {
-            System.out.println("(х_х) ОШИБКА! Формат математической операции, не удовлетворяет заданию.");
-            System.out.println("(◕‿◕) Повторите ввод...");
+        if (elements.length != 3 & elements.length < 3) {
+            System.out.println("Output: строка не является математической операцией.");
+            return;
+        }
+        if (elements.length != 3 & elements.length > 3) {
+            System.out.println("Output: формат математической операции, не удовлетворяет заданию – два операнда и один оператор.");
             return;
         }
         int a;
@@ -21,13 +23,11 @@ public class Calculator {
             a = Integer.parseInt(elements[0]);
             b = Integer.parseInt(elements[2]);
         } catch (NumberFormatException e) {
-            System.out.println("(х_х) ОШИБКА! Введено не корректное число.");
-            System.out.println("(◕‿◕) Повторите ввод...");
+            System.out.println("Output: введено не корректное число.");
             return;
         }
         if (a > 10 || b > 10) {
-            System.out.println("(х_х) ОШИБКА! Введённое число – болmше 10.");
-            System.out.println("(◕‿◕) Повторите ввод...");
+            System.out.println("Output: формат математической операции, не удовлетворяет заданию – только числа от 1 до 10 включительно.");
             return;
         }
         int result;
@@ -43,17 +43,15 @@ public class Calculator {
                 break;
             case "/":
                 if (b == 0) {
-                    System.out.println("(х_х) ОШИБКА! На ноль делить нельзя.");
-                    System.out.println("(◕‿◕) Повторите ввод...");
+                    System.out.println("Output: на ноль делить нельзя.");
                     return;
                 }
                 result = a / b;
                 break;
             default:
-                System.out.println("(х_х) ОШИБКА! Введён не корректный оператор.");
-                System.out.println("(◕‿◕) Повторите ввод...");
+                System.out.println("Output: введён не корректный оператор.");
                 return;
         }
-        System.out.println("(◕‿◕) Результат: " + result);
+        System.out.println("Output: " + result);
     }
 }
